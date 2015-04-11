039f31eba6bce1dc1ff951777c47e6c91b7946c4
Stefan Jönsson <rexas@g33k.se>
2015-04-11 22:51:21 +0200

    Can now request inventory information.

--- BigBrother.lua
+++ BigBrother.lua
@@ -20,9 +20,24 @@ BigBrother_ClassTalentLists["SHAMAN"] = {};
 BigBrother_ClassTalentLists["WARLOCK"] = {};
 BigBrother_ClassTalentLists["WARRIOR"] = {};
 
-BigBrother_TalenData = {};
+BigBrother_LastAddonMessageTime = 0;
+BigBrother_LastAddonMessageDelay = 2;
 
 function BigBrother_OnUpdate()
+	if BigBrother_LastAddonMessageTime + BigBrother_LastAddonMessageDelay <= GetTime()
+	then
+		if BigBrother_TableLength(BigBrother_AddonMessageBuffer) > 0
+		then
+			BigBrother_("Sending Addon message");
+			local data = table.remove(BigBrother_AddonMessageBuffer, 1);
+			
+			local messageID = data["messageID"];
+			local message = data["message"];
+			local channel = data["channel"];
+			BigBrother_LastAddonMessageTime = GetTime();
+			SendAddonMessage(messageID, message, channel);
+		end;
+	end
 end
 
 BigBrother_LastResponsSent = 0;
@@ -30,21 +45,30 @@ BigBrother_LastResponsSent = 0;
 function BigBrother_OnEvent()
 	if event == "ADDON_LOADED" and arg1 == "BigBrother"
 	then
-		
+		if not BigBrother_Database
+		then
+			BigBrother_("Loading default save.");
+			BigBrother_Database = {};
+			BigBrother_Database["Talent"] = {}
+			BigBrother_Database["Inventory"] = {}
+		end
 	end
 	
 	if event == "CHAT_MSG_ADDON"
 	then
-		-- /script SendAddonMessage("BigBrother_Talent_Request", "Rexwarrior", "GUILD");
+		-- /script BigBrother_SendAddonMessage("BigBrother_Talent_Request", "Rexwarrior", "GUILD");
+		--
+		-- TALENT EVENTS
+		--
 		if arg1 == "BigBrother_Talent_Request"
 		then
 			local requestName = arg2;
-			if UnitName("Player") == requestName or requestName == "all"
+			if UnitName("Player") == requestName --or requestName == "all" -- All might be a bit laggy.
 			then
 				if BigBrother_LastResponsSent + 2 < GetTime()
 				then
 					local playerClass, englishClass = UnitClass("player");
-					SendAddonMessage("BigBrother_Talent_Respond", englishClass .. "_" .. BigBrother_GetTalentString(), "GUILD");
+					BigBrother_SendAddonMessage("BigBrother_Talent_Respond", englishClass .. "_" .. BigBrother_GetTalentString(), "GUILD");
 					BigBrother_LastResponsSent = GetTime();
 				end;
 			end;
@@ -56,21 +80,95 @@ function BigBrother_OnEvent()
 			local split = __strsplit("_",msg);
 			local requestClass = split[1];
 			local talentString = split[2];
-			local msgFromUser = arg4;
+			local senderUsername = arg4;
 			
-			BigBrother_TalenData[msgFromUser] = {}
-			BigBrother_TalenData[msgFromUser]["class"] = requestClass;
-			BigBrother_TalenData[msgFromUser]["talent"] = talentString;
+			BigBrother_Database["Talent"][senderUsername] = {}
+			BigBrother_Database["Talent"][senderUsername]["class"] = requestClass;
+			BigBrother_Database["Talent"][senderUsername]["talent"] = talentString;
 			
-			if BigBrother_RequestList[msgFromUser] == true
+			if BigBrother_RequestList[senderUsername] == true
 			then
 				BigBrother_URL_frame_textBox:SetText(BigBrother_GetTalentURL(talentString, requestClass));
 				BigBrother_URL_frame:Show();
-				BigBrother_RequestList[msgFromUser] = false;
+				BigBrother_RequestList[senderUsername] = false;
+			end;
+		end;
+		
+		
+		
+		
+		--
+		-- INVENTORY EVENTS
+		--
+		
+		
+		
+		
+		-- /script BigBrother_SendAddonMessage("BigBrother_Inventory_Request", "Rexwarrior", "GUILD");
+		if arg1 == "BigBrother_Inventory_Request"
+		then
+			local requestName = arg2;
+			if UnitName("Player") == requestName --or requestName == "all" -- All might be a bit laggy.
+			then
+				if BigBrother_LastResponsSent + 2 < GetTime()
+				then
+					local playerClass, englishClass = UnitClass("player");
+					local d = BigBrother_MakeInventoryStrings(BigBrother_GetInventory());
+					for k,v in pairs(d)
+					do
+						--BigBrother_(d[k]);
+						BigBrother_SendAddonMessage("BigBrother_Inventory_Respond", d[k], "GUILD");
+					end;
+					BigBrother_LastResponsSent = GetTime();
+				end;
+			end;
+		end;
+		
+		
+		if arg1 == "BigBrother_Inventory_Respond"
+		then
+			local msg = arg2;
+			local split = __strsplit("_",msg);
+			local requestClass = split[1];
+			local talentString = split[2];
+			local senderUsername = arg4;
+			
+			if not BigBrother_Database["Inventory"][senderUsername]
+			then
+				BigBrother_Database["Inventory"][senderUsername] = {};
+				BigBrother_Database["Inventory"][senderUsername]["lastUpdate"] = 0;
+			end
+			
+			if BigBrother_Database["Inventory"][senderUsername]["lastUpdate"] + 10 < GetTime()
+			then
+				BigBrother_Database["Inventory"][senderUsername]["Items"] = {};
+			end
+			
+			BigBrother_Database["Inventory"][senderUsername]["lastUpdate"] = GetTime();
+			BigBrother_(msg);
+			local items = BigBrother_BreakInventoryString(msg);
+			--BigBrother_(items);
+			
+			for k,v in items
+			do
+				BigBrother_(v);
+				table.insert(BigBrother_Database["Inventory"][senderUsername]["Items"], v);
 			end;
 		end;
 	end;
 end
+
+
+BigBrother_AddonMessageBuffer = {};
+function BigBrother_SendAddonMessage(messageID, message, channel)
+	BigBrother_("Got request to send addon message");
+	local data = {};
+	data["messageID"] = messageID;
+	data["message"] = message;
+	data["channel"] = channel;
+	table.insert(BigBrother_AddonMessageBuffer, data);
+end;
+
 
 
 function BigBrother_GetTalentString()
@@ -107,9 +205,104 @@ function BigBrother_RequestTalentsFrom(unitName)
 	-- Anti spam, only once per 2 sec.
 	if BigBrother_LastRequest + 2 < GetTime()
 	then
-		SendAddonMessage("BigBrother_Talent_Request", unitName, "GUILD");
+		BigBrother_SendAddonMessage("BigBrother_Talent_Request", unitName, "GUILD");
 		BigBrother_RequestList[unitName] = true;
 	end
+end;
+
+--/script BigBrother_RequestTalentsFrom("Rexwarrior")
+function BigBrother_RequestInventoryFrom(unitName)
+	-- Anti spam, only once per 2 sec.
+	if BigBrother_LastRequest + 2 < GetTime()
+	then
+		BigBrother_SendAddonMessage("BigBrother_Inventory_Request", unitName, "GUILD");
+	end
+end;
+
+
+
+-- /script BigBrother_GetInventory()
+function BigBrother_GetInventory()
+	local returnData = {};
+	for bag = 0,4 do
+		for slot = 1,GetContainerNumSlots(bag) do
+			local texture, itemCount, locked, quality, readable = GetContainerItemInfo(bag,slot);
+			local item = GetContainerItemLink(bag,slot)
+			if item
+			then
+				--local found, _, itemString = string.find(item, "^|%x+|Hitem\:(.+)\:%[.+%]");
+				local a, b, color, itemID, name = string.find(item, "|c(%x+)|Hitem:(%d+:%d+:%d+:%d+)|h%[(.-)%]|h|r")
+				--BigBrother_(a .. " - " .. b .. " - " .. color .. " - " .. itemID .. " - " .. name);
+				if a
+				then
+					if not returnData[itemID]
+					then
+						returnData[itemID] = {};
+						returnData[itemID]["amount"] = 0;
+					end
+					returnData[itemID]["itemID"] = itemID;
+					returnData[itemID]["color"] = color;
+					returnData[itemID]["name"] = name;
+					returnData[itemID]["amount"] = returnData[itemID]["amount"] + itemCount;
+				end
+			end
+		end
+	end
+	return returnData;
+end
+
+-- /script BigBrother_MakeInventoryStrings(BigBrother_GetInventory())
+function BigBrother_MakeInventoryStrings(itemData)
+	local returnStrings = {};
+	local itemString = "";
+	for _,item in pairs(itemData)
+	do
+		local str = item["amount"]..";"..item["itemID"]..";"..item["color"]..";"..item["name"].."~";
+		--BigBrother_(item);
+		-- amount;itemID;color;name~
+		if string.len(str) + string.len(itemString) > 200
+		then
+			table.insert(returnStrings, itemString)
+			itemString = "";
+		end
+		itemString = itemString .. str;
+	end;
+	--BigBrother_(returnStrings);
+	return returnStrings;
+end
+
+-- /script local d = BigBrother_MakeInventoryStrings(BigBrother_GetInventory()); for k,v in pairs(d) do BigBrother_BreakInventoryString(d[k]) end;
+function BigBrother_BreakInventoryString(str)
+	local items = __strsplit("~", str);
+	--BigBrother_(items);
+	
+	local returnArr = {};
+	
+	for key,itemStr in pairs(items)
+	do
+		if string.len(itemStr) > 5
+		then
+			local item = __strsplit(";", itemStr);
+			--BigBrother_(key);
+			--BigBrother_(itemStr);
+			--BigBrother_(item);
+			local amount 	= item[1];
+			local itemID 	= item[2];
+			local color 	= item[3];
+			local name 		= item[4];
+			
+			--BigBrother_(amount.."x |c"..color.."|Hitem:"..itemID.."|h["..name.."]|h|r");
+			
+			local tmp = {}
+			tmp["amount"] = amount;
+			tmp["itemID"] = itemID;
+			tmp["color"] = color;
+			tmp["name"] = name;
+			
+			table.insert(returnArr, tmp);
+		end;
+	end
+	return returnArr;
 end;
 
 
@@ -117,12 +310,15 @@ end;
 
 
 
-
-
-
-
-
-
+function BigBrother_TableLength(t)
+	i = 0;
+	for k,v in pairs(t)
+	do
+		i = i + 1;
+	end
+	
+	return i;
+end
 
 
 
